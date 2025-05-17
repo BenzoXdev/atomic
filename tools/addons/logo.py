e1921dacfc0b092133ad1a1dee470ab076f8cec5
BenzoXdev <localtonet312@gmail.com>
2025-05-17 09:24:53 -0400

    Initial commit

---
+++ tools/addons/logo.py
@@ -0,0 +1,14 @@
+# Import modules
+from colorama import Fore
+
+# Logo
+print(rf"""{Fore.MAGENTA}
+ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄   ▄▄ ▄▄▄ ▄▄▄▄▄▄▄ 
+█       █       █       █  █▄█  █   █       █
+█   ▄   █▄     ▄█   ▄   █       █   █       █
+█  █▄█  █ █   █ █  █ █  █       █   █     ▄▄█
+█       █ █   █ █  █▄█  █       █   █    █   
+█   ▄   █ █   █ █       █ ██▄██ █   █    █▄▄ 
+█▄▄█ █▄▄█ █▄▄▄█ █▄▄▄▄▄▄▄█▄█   █▄█▄▄▄█▄▄▄▄▄▄▄█
+                                Created by BenzoXdev
+{Fore.RESET}""")
